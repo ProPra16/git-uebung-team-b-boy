7121d6412f72648e3aaf22c67722be9bdcd5e3b5
Björn Ebbinghaus <bjoern@ebbinghaus.me>
2016-06-16 09:41:45 +0200

    added button logic

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,7 +1,22 @@
 package de.hhu.propra16;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtonsController {
+    public Label dateLabel;
+
+    public void showDate(ActionEvent actionEvent) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date now = new Date();
+        String dateString = sdf.format(now);
+
+        dateLabel.setText(dateString);
+    }
 }
