c6150c509afaf41b472a1e4c3cb1f890c34e5c23
Asri <asri.ferati@hhu.de>
2016-06-20 21:45:59 +0200

    Set AlertButton

--- src/de/hhu/propra16/FiveButtonsController.java
+++ src/de/hhu/propra16/FiveButtonsController.java
@@ -1,6 +1,8 @@
 package de.hhu.propra16;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 import java.text.SimpleDateFormat;
@@ -11,12 +13,18 @@ import java.util.Date;
  */
 public class FiveButtonsController {
     public Label dateLabel;
+    public Button TooManyButton;
+
+    public void setAlert(ActionEvent event) {
+        Alert Warnung=new Alert(Alert.AlertType.ERROR);
+        Warnung.setContentText("Too Many Buttons");
+        Warnung.showAndWait();
+    }
 
     public void showDate(ActionEvent actionEvent) {
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
         Date now = new Date();
         String dateString = sdf.format(now);
-
         dateLabel.setText(dateString);
     }
 }
