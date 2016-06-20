6dd04562d5af9d8b23adddc7b7a4b2db0426b673
Asri <asri.ferati@hhu.de>
2016-06-20 21:45:28 +0200

    Set AlertButton

--- src/de/hhu/propra16/FiveButtons.java
+++ src/de/hhu/propra16/FiveButtons.java
@@ -6,32 +6,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 /**
  * Created by Tobias Witt on 06.06.16.
  */
 public class FiveButtons extends Application {
-    public Button TooManyButtons=new Button("Button a)");
-
 
     public static void main(String[] args) {
         launch(args);
-    }
-
-    public void setAlert() {
-        TooManyButtons.setOnAction(event -> {
-            Alert Warnung=new Alert(Alert.AlertType.ERROR);
-            Warnung.setContentText("Too Many Buttons");
-        });
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/five_buttons" +
                 ".fxml"));
-        root.getChildrenUnmodifiable().add(TooManyButtons);
-        setAlert();
         primaryStage.setTitle("Five Buttons");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
